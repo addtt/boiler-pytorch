0ab1768a9f23a39a2e9766eaf5867662cbf9ec3e
Andrea Dittadi <andrea.dittadi@gmail.com>
2020-04-18 22:48:17 +0200

    Add util method to compute grad norm

--- boilr/_version.py
+++ boilr/_version.py
@@ -1,4 +1,4 @@
 __all__ = ["__version__", "__version_info__"]
 
-__version__ = "0.6.5"
+__version__ = "0.6.6"
 __version_info__ = tuple(int(x) for x in __version__.split("."))
--- boilr/utils.py
+++ boilr/utils.py
@@ -1,4 +1,5 @@
 import datetime
+import math
 import os
 import random
 import re
@@ -11,6 +12,7 @@ import torch.utils.data
 from .options import get_options
 
 __sentinel = object()
+
 
 def print_num_params(model, max_depth=__sentinel):
 
@@ -42,6 +44,38 @@ def print_num_params(model, max_depth=__sentinel):
         print("{:8d}  {}".format(n_par, n))
     print("  - Total trainable parameters:", num_params_tot)
     print("---------\n")
+
+
+def grad_norm(parameters, norm_type=2):
+    r"""Compute gradient norm of an iterable of parameters.
+
+    The norm is computed over all gradients together, as if they were
+    concatenated into a single vector. Gradients are modified in-place.
+    This code is based on torch.nn.utils.clip_grad_norm_(), with minor
+    modifications.
+
+    Arguments:
+        parameters (Iterable[Tensor] or Tensor): an iterable of Tensors or a
+            single Tensor that will have gradients normalized
+        norm_type (float or int): type of the used p-norm. Can be ``'inf'`` for
+            infinity norm.
+
+    Returns:
+        Total norm of the parameters (viewed as a single vector).
+    """
+    if isinstance(parameters, torch.Tensor):
+        parameters = [parameters]
+    parameters = list(filter(lambda p: p.grad is not None, parameters))
+    norm_type = float(norm_type)
+    if norm_type == math.inf:
+        total_norm = max(p.grad.data.abs().max() for p in parameters)
+    else:
+        total_norm = 0
+        for p in parameters:
+            param_norm = p.grad.data.norm(norm_type)
+            total_norm += param_norm.item() ** norm_type
+        total_norm = total_norm ** (1. / norm_type)
+    return total_norm
 
 
 def set_rnd_seed(seed, aggressive=False):
@@ -100,23 +134,20 @@ def named_leaf_modules(module):
 
 
 def checkpoints_in_folder(folder):
-    """
+    r"""Find checkpoints in a folder.
+
     Find checkpoint files in the speficied folder, return a list of file names
     and a list of integers corresponding to the time steps when the checkpoints
     were saved. These lists have the same length, they have a one-to-one
     correspondence, and they are sorted by number.
 
-    Parameters
-    ----------
-    folder: str
+    Arguments:
+        folder (str)
 
-    Returns
-    -------
-    filenames: list of str
-        File names of checkpoints. The full path is given by joining the
-        input argument folder with these file names.
-    numbers: list of int
-
+    Returns:
+        filenames (list of str): File names of checkpoints. The full path is
+        obtained by joining the input argument folder with these file names.
+        numbers (list of int)
     """
 
     def is_checkpoint_file(f):
